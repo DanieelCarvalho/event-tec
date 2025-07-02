2fb006ac5983168963a1934395f376fe3cc5f0f2
daniel <daniescg2012@gmail.com>
2025-07-02 09:26:23 -0300

    first commit

---
+++ src/main/java/br/com/api/eventos_tec/domain/coupon/Coupon.java
@@ -0,0 +1,40 @@
+package br.com.api.eventos_tec.domain.coupon;
+
+import java.sql.Date;
+import java.util.UUID;
+
+import br.com.api.eventos_tec.domain.event.Event;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "event-coupon")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coupon {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+
+    private Integer discount;
+
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+}
